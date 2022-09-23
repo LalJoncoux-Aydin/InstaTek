35d8f40b99e512115d3ee1e2b8ef72933af851e5
Oscar Rénier <oscar.renier@gmail.com>
2022-09-23 16:30:29 +0200

    [ADD]: Firebase configuration for web, android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instatek/responsive/mobile_screen_layout.dart';
-import 'package:instatek/responsive/responsive_layout_screen.dart';
-import 'package:instatek/responsive/web_screen_layout.dart';
-import 'package:instatek/screens/login_screen.dart';
 import 'package:instatek/screens/register_screen.dart';
 import 'package:instatek/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyDdn1q0EK9RU1JMBtqPhfNNjMGUQV5TieE',
+            appId: '1:254429523809:web:160dbab7037581ac0d5f20',
+            messagingSenderId: '254429523809',
+            projectId: 'instatek-6fa75',
+            storageBucket: 'instatek-6fa75.appspot.com'));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -22,8 +32,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-     // home: ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout()),
-      home: RegisterScreen(),
+      // home: ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout()),
+      home: const RegisterScreen(),
     );
   }
 }
