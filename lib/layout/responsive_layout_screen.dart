2bf2775e84831d09ca88c559cddfd546bd0cf551
Lal Joncoux <lal.joncoux-aydin@epitech.eu>
2022-10-27 00:56:21 +0200

    [ADD] Custom loading creen - admin view setup

--- lib/layout/responsive_layout_screen.dart
+++ lib/layout/responsive_layout_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:instatek/models/user.dart' as model;
 import 'package:instatek/providers/user_provider.dart';
 import 'package:instatek/utils/global_variables.dart';
 import 'package:provider/provider.dart';
+
+import '../widgets/custom_loading_screen.dart';
 
 class ResponsiveLayout extends StatefulWidget {
   const ResponsiveLayout({Key? key, required this.webScreenLayout, required this.mobileScreenLayout, required this.adminScreenLayout}) : super(key:key);
@@ -9,23 +12,30 @@ class ResponsiveLayout extends StatefulWidget {
   final Widget mobileScreenLayout;
   final Widget adminScreenLayout;
 
-
   @override
   State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
 }
 
 class _ResponsiveLayoutState extends State<ResponsiveLayout> with SingleTickerProviderStateMixin {
   late UserProvider userProvider;
+  late model.User myUser;
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    addData();
+    setupUser();
   }
 
-  void addData() async {
+  void setupUser() async {
     userProvider = Provider.of(context, listen: false);
     await userProvider.refreshUser();
+    if (userProvider.isUser == true) {
+      setState(() {
+        myUser = userProvider.getUser;
+        _isLoading = true;
+      });
+    }
   }
 
   @override
@@ -35,15 +45,21 @@ class _ResponsiveLayoutState extends State<ResponsiveLayout> with SingleTickerPr
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        if (constraints.maxWidth > webScreenSize && userProvider.getUser.isAdmin == false) {
-          return widget.webScreenLayout;
-        } else if (constraints.maxWidth > webScreenSize && userProvider.getUser.isAdmin == true) {
-          return widget.adminScreenLayout;
-        }
-        return widget.mobileScreenLayout;
-      },
-    );
+    if (_isLoading == false) {
+      setupUser();
+      return const CustomLoadingScreen();
+    } else {
+      return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > webScreenSize && myUser.isAdmin == false) {
+            return widget.webScreenLayout;
+          } else
+          if (constraints.maxWidth > webScreenSize && myUser.isAdmin == true) {
+            return widget.adminScreenLayout;
+          }
+          return widget.mobileScreenLayout;
+        },
+      );
+    }
   }
 }
---
+++ lib/widgets/custom_loading_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instatek/utils/colors.dart';
+
+class CustomLoadingScreen extends StatefulWidget {
+  const CustomLoadingScreen({super.key});
+
+  @override
+  CustomLoadingScreenState createState() => CustomLoadingScreenState();
+}
+
+class CustomLoadingScreenState extends State<CustomLoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Loading"),
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(greenColor),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
