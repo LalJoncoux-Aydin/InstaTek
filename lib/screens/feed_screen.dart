76744972a385cf142a4ac2ed7beb58ed29d08814
Oscar Rénier <oscar.renier@gmail.com>
2022-10-28 17:13:08 +0200

    [ADD]: Posts, comments and likes added in feed

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,31 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instatek/utils/colors.dart';
+import 'package:instatek/utils/global_variables.dart';
 import 'package:instatek/widgets/post_card.dart';
 
-class FeedScreen extends StatelessWidget {
-  const FeedScreen({super.key});
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({Key? key}) : super(key: key);
 
   @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        centerTitle: false,
-        title: SvgPicture.asset(
-          'assets/instatek_logo.svg',
-          color: primaryColor,
-          height: 32,
-        ),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.messenger_outline,
-              ),)
-        ],
+      backgroundColor: width > webScreenSize ? webBackgroundColor : mobileBackgroundColor,
+      appBar: width > webScreenSize
+          ? null
+          : AppBar(
+              backgroundColor: mobileBackgroundColor,
+              centerTitle: false,
+              title: SvgPicture.asset(
+                'assets/instatek_logo.svg',
+                color: primaryColor,
+                height: 32,
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Icons.messenger_outline_rounded,
+                    color: primaryColor,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext ctx, int index) => Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: width > webScreenSize ? width * 0.3 : 0,
+                vertical: width > webScreenSize ? 15 : 0,
+              ),
+              child: PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              ),
+            ),
+          );
+        },
       ),
-      body: const PostCard(),
     );
   }
 }
