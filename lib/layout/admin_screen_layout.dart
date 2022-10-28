73571f89165de56f3d02a6568c4ec4e34272a4bb
Camille <camille.sayous@epitech.eu>
2022-10-28 23:09:08 +0200

    [ADD] user list and delete user function

--- lib/layout/admin_screen_layout.dart
+++ lib/layout/admin_screen_layout.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instatek/models/user.dart' as model;
 import 'package:instatek/providers/user_provider.dart';
 import 'package:instatek/screens/admin/admin_history_screen.dart';
 import 'package:instatek/screens/admin/admin_post_screen.dart';
 import 'package:instatek/screens/admin/admin_user_screen.dart';
+import 'package:instatek/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 class AdminScreenLayout extends StatefulWidget {
@@ -46,6 +48,15 @@ class AdminScreenLayoutState extends State<AdminScreenLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: blueColor,
+        centerTitle: true,
+        title: SvgPicture.asset(
+                'assets/instatek_logo.svg',
+                color: primaryColor,
+                height: 32,
+              ),
+        ),
       body: Row(
         children: <Widget>[
           NavigationRail(
@@ -81,7 +92,7 @@ class AdminScreenLayoutState extends State<AdminScreenLayout> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('selectedIndex: $_selectedIndex'),
+               // Text('selectedIndex: $_selectedIndex'),
                 if (_selectedIndex == 0) const AdminPostScreen() else const SizedBox(),
                 if (_selectedIndex == 1) const AdminUserScreen() else const SizedBox(),
                 if (_selectedIndex == 2) const AdminHistoryScreen() else const SizedBox(),
