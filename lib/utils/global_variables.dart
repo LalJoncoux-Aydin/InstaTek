16c8085ff8f1f4bb3781899b80ba0e452d2ee7a9
Lal Joncoux <lal.joncoux-aydin@epitech.eu>
2022-11-13 16:03:44 +0100

    [UP] Webscreen size

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -5,7 +5,7 @@ import '../screens/user/favorite/favorite_screen.dart';
 import '../screens/user/profile/profile_screen.dart';
 import '../screens/user/search/search_screen.dart';
 
-int webScreenSize = 600;
+int webScreenSize = 1366;
 const String defaultAvatarUrl = 'https://cdn-icons-png.flaticon.com/512/847/847969.png';
 
 List<Widget> homeScreenItems = <Widget>[
